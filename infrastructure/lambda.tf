38cc79199de7b94419d78878b91b60695f6d6e58
dstilesr <d.stiles.r@hotmail.com>
2023-05-02 19:25:47 -0500

    Fix typo

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -25,7 +25,7 @@ resource "aws_lambda_function" "mangum_func" {
   function_name    = "mangum-example-function"
   role             = aws_iam_role.mangum_func_role.arn
   runtime          = "python3.9"
-  package_type     = "zip"
+  package_type     = "Zip"
   filename         = local.function_file
   source_code_hash = filebase64sha256(local.function_file)
   memory_size      = var.lambda_memory
