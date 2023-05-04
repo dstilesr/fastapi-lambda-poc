d33de45e0826d2022ffa9f6dce46bbef198046e4
dstilesr <d.stiles.r@hotmail.com>
2023-05-04 08:37:46 -0500

    Put IAM resources in separate file

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -3,24 +3,6 @@ locals {
   function_file = "../lambda-src.zip"
 }
 
-resource "aws_iam_role" "mangum_func_role" {
-  name = "MangumFunctionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = "sts:AssumeRole"
-
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
 resource "aws_lambda_function" "mangum_func" {
   function_name    = "mangum-example-function"
   role             = aws_iam_role.mangum_func_role.arn
@@ -36,28 +18,4 @@ resource "aws_lambda_function" "mangum_func" {
       STAGE_NAME = var.stage_name
     }
   }
-}
-
-resource "aws_iam_policy" "write_logs" {
-  name = "MangumLogsPolicy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = ["arn:aws:logs:*:*:*"]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "logs_attach" {
-  policy_arn = aws_iam_policy.write_logs.arn
-  role       = aws_iam_role.mangum_func_role.name
 }
